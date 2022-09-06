ca177b7150b2b72dd1345c76ff0909e8933efd3d
WillGolden80742 <william80742@gmail.com>
2022-09-05 22:56:17 -0300

    Update index.php

--- index.php
+++ index.php
@@ -7,18 +7,19 @@
 <head>
 </head>    
 <body>
-<?php
-    $session = new Sessions();
-    use Goutte\Client;
-    use Symfony\Component\HttpClient\HttpClient;
-    $filter = new HTMLFilter("");
-    $site = file_get_contents("http://www.obt.inpe.br/OBT/assuntos/programas/amazonia/prodes");
-    $update = "<center>(*".$filter->getContentHTML($site,"Atualizado",")")."</center>";
-    $site = $filter->getContentHTMLByTag($site,"table class=\"plain");
-    echo $site;
-    echo $update;
+    <?php
+    
+        #use Goutte\Client;
+        #use Symfony\Component\HttpClient\HttpClient;
+        $session = new Sessions();
+        $filter = new HTMLFilter("");
+        $site = file_get_contents("http://www.obt.inpe.br/OBT/assuntos/programas/amazonia/prodes");
+        $update = "<center>(*".$filter->getContentHTML($site,"Atualizado",")")."</center>";
+        $site = $filter->getContentHTMLByTag($site,"table class=\"plain");
+        echo $site;
+        echo $update;
 
-?>
+    ?>
 
 
 </body>
