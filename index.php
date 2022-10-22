5f6c1e49754fb693d993b985162cea77e14fc28e
WillGolden80742 <william80742@gmail.com>
2022-10-21 22:06:39 -0300

    Update index.php

--- index.php
+++ index.php
@@ -440,7 +440,7 @@ require 'global.php';
 
     <div id="estateMap" class="estateMap" height="450">
 
-            <svg version="1.1" id="svg-map" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="100%" height="100%" viewBox="0 0 450 460" enable-background="new 0 0 450 460" xml:space="preserve">
+            <svg version="1.1" id="svg-map" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="100%" viewBox="0 0 450 460" enable-background="new 0 0 450 460" xml:space="preserve">
                 <g>
                 <a onclick="selectMap('tocantins');" id="tocantins" xlink:href="#tocantins" >
                 <title>Tocantins</title>
