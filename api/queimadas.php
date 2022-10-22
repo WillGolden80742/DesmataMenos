1bad93bc1acf4e268768e03425b1773ef0155948
WillGolden80742 <william80742@gmail.com>
2022-10-22 14:09:05 -0300

    Update queimadas.php

--- api/queimadas.php
+++ api/queimadas.php
@@ -11,6 +11,7 @@
         $estados = explode(",","acre,alagoas,amapa,amazonas,bahia,ceara,distrito_federal,espirito_santo,goias,maranhao,mato_grosso,mato_grosso_do_sul,minas_gerais,para,paraiba,parana,pernambuco,piaui,rio_de_janeiro,rio_grande_do_norte,rio_grande_do_sul,rondonia,roraima,santa_catarina,sao_paulo,sergipe,tocantins");
     }
 
+    #criandoLocale
     $conjuntoLocale="";
     $datetime = new DateTime();
     $dateFormated = $datetime->format('m/Y');
