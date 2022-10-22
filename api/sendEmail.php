d35ae3c0a0ccfa27a13312c1141f48b9553fe646
WillGolden80742 <william80742@gmail.com>
2022-10-22 18:10:44 -0300

    Update sendEmail.php

--- api/sendEmail.php
+++ api/sendEmail.php
@@ -3,7 +3,7 @@
     $arrURL = explode("/",$currentURL);
     $sizeArrURL = count ($arrURL)-1;
     $currentURL = str_replace($arrURL[$sizeArrURL],"",$currentURL);
-    $content = file_get_contents($currentURL."table.php?locale=".$_GET['locale']);
+    $content = file_get_contents($currentURL."queimadas.php?locale=".$_GET['locale']);
     echo $content;
 
     mail('william80742@gmail.com',"Dado de ".$_GET['locale'],$content);
