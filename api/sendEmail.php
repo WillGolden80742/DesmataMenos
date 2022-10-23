720cc42849900b3cecc2c12054e0fb8a923d8f51
WillGolden80742 <william80742@gmail.com>
2022-10-22 21:48:19 -0300

    Update sendEmail.php

--- api/sendEmail.php
+++ api/sendEmail.php
@@ -1,4 +1,4 @@
 <?php
     $headers = "Content-Type: text/html; charset=UTF-8\r\n";
-    mail($_POST['mail'],"Dado de ",$_POST['content'],$headers);
+    mail($_POST['mail'],"Dado de ".$_POST['locale'],$_POST['content'],$headers);
 ?>
