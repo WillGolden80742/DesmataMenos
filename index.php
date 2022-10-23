ba78f7c3d864f2f9b159caa06657bf124bb67e2e
WillGolden80742 <william80742@gmail.com>
2022-10-22 22:04:05 -0300

    Update index.php

--- index.php
+++ index.php
@@ -218,11 +218,11 @@
                 loading ();
                 document.getElementById("desmatamentoTable").style.overflowY = "scroll";
                 if (estado !== "") {
+                    document.getElementById('select_mail').disabled = false;
                     tabela ();
                 } else {
                     tabelaTodosEstado();
                 }
-                document.getElementById('select_mail').disabled = false;
                 document.getElementById("styleTable").innerHTML+=".desmatamentoTable { transition:0.5s; box-shadow:none; } .desmatamentoTable:hover { transition:0.5s; box-shadow: inset 0px 0px 20px rgb(0,0,0,1); }";
             } else if (select_dado == "grafico") {
                 document.getElementById('select_mail').disabled = true;
