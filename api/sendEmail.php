a0135b17f79811aa490328c984da6dac0365bbba
WillGolden80742 <william80742@gmail.com>
2022-10-22 21:22:57 -0300

    Update sendEmail.php

--- api/sendEmail.php
+++ api/sendEmail.php
@@ -1,8 +1,5 @@
 <?php
     $headers = "Content-Type: text/html; charset=UTF-8\r\n";
     $locale = "";
-    if (isset($_POST['locale'])) {
-        $locale=$_POST['locale'];
-    }
-    mail($_POST['mail'],"Dado de ".$locale,$_POST['content'],$headers);
+    mail($_POST['mail'],"Dado de ".$_POST['locale'],$_POST['content'],$headers);
 ?>
