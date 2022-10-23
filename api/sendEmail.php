49d206d41e766bd268462d3f37f5eadb349e609f
WillGolden80742 <william80742@gmail.com>
2022-10-23 13:43:11 -0300

    removendo error de formatação

--- api/sendEmail.php
+++ api/sendEmail.php
@@ -1,5 +1,6 @@
-<?php
+<?php   
+    
     $headers = "Content-Type: text/html; charset=UTF-8\r\n";
-    mail($_POST['mail'],"Dado de ".$_POST['locale'],$_POST['content'],$headers);
-    echo $_POST['mail']."\n Dado de ".$_POST['locale'].$_POST['content'];
+    $content = str_replace("\"","'",$_POST['content']);
+    mail($_POST['mail'],"Dado de ".$_POST['locale'],$content,$headers);
 ?>
