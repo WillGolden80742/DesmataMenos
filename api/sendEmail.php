2261868e74105787144058777f9d3b848e9a3ee9
WillGolden80742 <william80742@gmail.com>
2022-10-23 22:12:28 -0300

    Update sendEmail.php

--- api/sendEmail.php
+++ api/sendEmail.php
@@ -1,7 +1,8 @@
 <?php   
     require 'Controller/Table.php';
     $headers = "Content-Type: text/html; charset=UTF-8\r\n";
-    if (strlen(new Table($_POST['content'])) == 0) {
+    $content = $_POST['content'];
+    if (strlen(new Table($content)) == 0) {
 		mail($_POST['mail'],"Dado de ".$_POST['locale'],$content,$headers);
     }
 ?>
