c77e8e044348dd23204ce6154ca0af83c63b0bbc
WillGolden80742 <91426752+WillGolden80742@users.noreply.github.com>
2022-10-26 12:58:55 -0300

    Test

--- api/sendEmail.php
+++ api/sendEmail.php
@@ -11,6 +11,6 @@
         echo json_encode(Array("Envio com sucesso!",true));
 		mail($mail,"Dado de ".$locale,"<center><h2>".$locale."</h2>".$content."</center>",$headers);
     } else {
-        echo json_encode(Array("Documento em formato incorreto!".$_POST['content']   ,false));
+        echo json_encode(Array("Documento em formato incorreto!".new Table($content)) ,false));
     }
 ?>
