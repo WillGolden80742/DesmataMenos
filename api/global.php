a5b2c1cfcd36efa26181af60be17954309802b37
WillGolden80742 <91426752+WillGolden80742@users.noreply.github.com>
2022-10-26 16:07:58 -0300

    Set locale time

--- api/global.php
+++ api/global.php
@@ -3,7 +3,7 @@
     $iniEnv = new Environment();
     $iniEnv->load(__DIR__);
     #setlocale(LC_ALL, 'pt_BR', 'pt_BR.utf-8', 'pt_BR.utf-8', 'portuguese');
-    #date_default_timezone_set('America/Sao_Paulo');
+    date_default_timezone_set('America/Sao_Paulo');
     #show erros in php
     ini_set('display_errors', 1);
 ?>
